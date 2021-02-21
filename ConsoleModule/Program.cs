6f6114d3dfea39277935efaa2d5374dc2f2e2b2b
AlexBim <centuriin@yandex.ru>
2021-02-22 00:13:11 +0300

    basic

--- ConsoleModule/Program.cs
+++ ConsoleModule/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuickMathsBL;
 
 namespace ConsoleModule
 {
@@ -11,13 +12,12 @@ namespace ConsoleModule
         static void Main(string[] args)
         {
             //TODO
-            Console.WriteLine("Hello, Wrld");
-            int age = Convert.ToInt32(Console.ReadLine());
-            var number = 123444444;
-            string semenName = "Semen";
-            Console.WriteLine(number);
-            string name = Console.ReadLine();
-            Console.WriteLine($"Age is {age}\nName is {name}");
+            SimpleFunction mySimpleFunc = new SimpleFunction(1, SimpleFunction.SimpleFunc.LinearFunction);
+            SimpleFunction mySimpleFunc2 = new SimpleFunction(2);
+            SimpleFunction mySimpleFunc3 = new SimpleFunction(SimpleFunction.E);
+            Console.WriteLine($"Type: {mySimpleFunc.GetTypeFunction}\n Kof: {mySimpleFunc.GetKofFunction}");
+            Console.WriteLine($"Type: {mySimpleFunc2.GetTypeFunction}\n Kof: {mySimpleFunc2.GetKofFunction}");
+            Console.WriteLine($"Type: {mySimpleFunc3.GetTypeFunction}\n Kof: {mySimpleFunc3.GetKofFunction}");
             Console.ReadKey();
         }
     }
--- QuickMathsBL/SimpleFunction.cs
+++ QuickMathsBL/SimpleFunction.cs
@@ -8,5 +8,43 @@ namespace QuickMathsBL
 {
     public class SimpleFunction
     {
+        public enum SimpleFunc
+        {
+            Unkown,
+            LinearFunction,
+            PowerFunction
+        }
+
+        private static double e = Math.E;
+        private static double pi = Math.PI;
+
+        private double kof;
+        private string typeFunction;
+
+        //Contructor
+        public SimpleFunction(double _kof, SimpleFunc _typeSimpleFunc = SimpleFunc.Unkown)
+        {
+            kof = _kof;
+            typeFunction = _typeSimpleFunc.ToString();
+        }
+
+        public double GetKofFunction
+        {
+            get { return kof; }
+        }
+
+        public string GetTypeFunction
+        {
+            get { return typeFunction; }
+        }
+
+        public static double E
+        {
+            get { return e; }
+        }
+        public static double Pi
+        {
+            get { return pi; }
+        }
     }
 }
