4286750916e5ab9ea14c8117fdcf3a4b6ea5bcf0
AlexBim <centuriin@yandex.ru>
2021-02-23 15:42:34 +0300

    add new fucntion for ConsoleHelper

--- QuickMathsBL/ConsoleHelper.cs
+++ QuickMathsBL/ConsoleHelper.cs
@@ -8,22 +8,23 @@ namespace QuickMathsBL
 {
     public abstract class ConsoleHelper
     {
-        public enum Command
-        {
-            HELP,
-            EXIT,
-            CLEAR,
-            CREATE
-        }
 
-        private static Dictionary<string, string> commandDict = new Dictionary<string, string>()
+        private static Dictionary<string, Action> commandDict = new Dictionary<string, Action>()
         {
-            {"HELP", "Write all command in console." },
-            {"EXIT", "Close ConsoleHelper." },
-            {"CLEAR", "Clear console." },
-            {"CREATE", "This function can create different object of classes." }
+            {"HELP|Write all command in console.", Help },
+            {"EXIT|Close ConsoleHelper.", Exit },
+            {"CLEAR|Clear console.", (Action)(()=>Console.Clear()) },
+            {"CREATE|This function can create different object of classes.", Create },
+            {"RETURN VARIABLE|Write list of all created variable", ReturnVariable}
         };
 
+        private static Dictionary<string, string> classesDict = new Dictionary<string, string>()
+        {
+            {"SF", "SimpleFunction" },
+            {"F", "Function" }
+        };
+
+        private static Dictionary<string, object> variableDict = new Dictionary<string, object>();
 
         private static bool life = true;
 
@@ -47,15 +48,20 @@ namespace QuickMathsBL
 
         private static void SearchCommand(string command)
         {
-            if (command.ToUpper() == Command.HELP.ToString())
-                Help();
-            else if (command.ToUpper() == Command.CLEAR.ToString())
-                Console.Clear();
-            else if (command.ToUpper() == Command.EXIT.ToString())
-                Exit();
-            else if (command.ToUpper() == Command.CREATE.ToString())
-                Create();
-            else
+            bool find = false;
+            foreach(var i in commandDict)
+            {
+                string[] info = i.Key.Split('|');
+                string nameCommand = info[0];
+                if(command.ToUpper() == nameCommand)
+                {
+                    find = true;
+                    i.Value();
+                    break;
+                }
+            }
+
+            if(!find)
             {
                 Console.WriteLine($"Command {command} incorrect.\n");
                 return;
@@ -63,17 +69,63 @@ namespace QuickMathsBL
             Console.WriteLine($"Command {command} is complete\n");
         }
 
+        private static void ReturnVariable()
+        {
+            foreach(var i in variableDict)
+            {
+                Console.WriteLine($"\tVariable name: {i.Key} Class: {i.Value.GetType()}\n");
+            }
+        }
+
         private static void Create()
         {
+            foreach (var i in classesDict)
+                Console.WriteLine($"\tClass key: {i.Key}, class name: {i.Value}\n");
+            Console.Write("\t\tWrite need key: ");
+            string className = Console.ReadLine();
+            bool find = false;
+            foreach (var i in classesDict)
+            {
+                if (className.ToUpper() == i.Key)
+                {
+                    find = true;
+                    break;
+                }
+            }
+            if (find)
+            {
+                switch (className.ToUpper())
+                {
+                    case "SF":
+                        Console.Write("\t\tWrite name of object class SF: ");
+                        string variableName = Console.ReadLine();
+                        Console.Write("\t\tWrite koef: ");
+                        int koef = Int32.Parse(Console.ReadLine());
+                        SimpleFunction itemSF = new SimpleFunction(koef);
+                        variableDict.Add(variableName, itemSF);
+                        break;
+                    case "F":
+                        Console.Write("\t\tWrite name of object class F: ");
+                        string variableName1 = Console.ReadLine();
+                        Console.Write("\t\tWrite koef: ");
+                        int koef1 = Int32.Parse(Console.ReadLine());
+                        Function itemF = new Function(koef1);
+                        variableDict.Add(variableName1, itemF);
+                        break;
+                }
 
+            }
+            else
+                Console.WriteLine($"\tKey {className} incorrect.\n");
         }
 
         private static void Help()
         {
             foreach (var i in commandDict)
             {
-                Console.WriteLine($"\tCommand {i.Key}");
-                Console.WriteLine($"\t\t{i.Value}\n");
+                string[] info = i.Key.Split('|');
+                Console.WriteLine($"\tCommand {info[0]}");
+                Console.WriteLine($"\t\t{info[1]}\n");
             }
         }
 
