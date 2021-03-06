10dc93d3a9783d828a1511538303f68717c4e608
AlexBim <centuriin@yandex.ru>
2021-03-06 23:56:07 +0300

    derivative class

---
+++ QuickMathsBL/Derivative.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMathsBL
+{
+    public static class Derivative  //Производная
+    {
+
+        //TODO
+
+    }
+}
