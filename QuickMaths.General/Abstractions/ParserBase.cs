4e78fab2d5f3a83171f964b5a0d0330b81e7e870
centuriin <centuriin@yandex.ru>
2022-05-28 20:48:37 +0300

    #20 Integration with parser

--- QuickMaths.General/Abstractions/ParserBase.cs
+++ QuickMaths.General/Abstractions/ParserBase.cs
@@ -12,11 +12,24 @@ namespace QuickMaths.General.Abstractions;
 /// <typeparam name="TEntity" xml:lang = "ru">
 /// Сущность, которую парсер должен вернуть после разбора строки.
 /// </typeparam>
-public abstract class ParserBase<TEntity> where TEntity : IArithmeticable
+public abstract class ParserBase<TEntity>
 {
     // Вариативно, сигнатура может быть другая
     // Метод разбивающий строку по операторам общий для всех парсеров
     protected void SplitOnOperators() => throw new NotImplementedException();
+
+    /// <summary xml:lang = "ru">
+    /// Создает дерево выражений с сущностями типа <typeparamref name="TEntity"/> по входной строке.
+    /// </summary>
+    /// <param name="input" xml:lang = "ru">
+    /// Входная строка, которую нужно распарсить.
+    /// </param>
+    /// <returns xml:lang = "ru">
+    /// Дерево выражений, наполненное сущностями типа <typeparamref name="TEntity"/>.
+    /// </returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    public abstract ITreeExpression<TEntity> BuildExpression(string input);
 
     /// <summary xml:lang = "ru">
     /// Разбирает входную строку и пытается привести её к объекту типа <typeparamref name="TEntity"/>.
@@ -24,11 +37,11 @@ public abstract class ParserBase<TEntity> where TEntity : IArithmeticable
     /// <param name="inputString" xml:lang = "ru">
     /// Входная строка, которую нужно распарсить.
     /// </param>
-    /// <exception cref="FormatException"/>
-    /// <exception cref="ArgumentNullException"/>
     /// <returns xml:lang = "ru">
     /// Объект типа <typeparamref name="TEntity"/>.
-    /// </returns>
+    /// </returns>    
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
     public abstract TEntity Parse(string inputString);
 
     /// <summary xml:lang = "ru">
@@ -48,21 +61,20 @@ public abstract class ParserBase<TEntity> where TEntity : IArithmeticable
     public abstract bool TryParse(string inputString, out TEntity result);
 
     /// <summary xml:lang = "ru">
-    /// Запускает метод <see cref="Parse(string)"/>, передавая ему входную строку, на пуле потоков.
+    /// Асинхронно разбирает входную строку и пытается привести её к объекту типа <typeparamref name="TEntity"/>.
     /// </summary>
     /// <param name="inputString" xml:lang = "ru">
     /// Входная строка, которую нужно распарсить.
-    /// </param>
+    /// </param>  
     /// <param name="token" xml:lang = "ru">
     /// Токен отмены операции.
     /// </param>
     /// <returns xml:lang = "ru">
-    /// Задача типа <see cref="Task{TResult}"/>.
+    /// Задача типа <see cref="Task{TResult}"/>, результат которой
+    /// является объект типа <typeparamref name="TEntity"/>.
     /// </returns>
-    public virtual Task<TEntity> ParseAsync(string inputString, CancellationToken token)
-    {
-        token.ThrowIfCancellationRequested();
-
-        return Task.Run(() => Parse(inputString));
-    }
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OperationCanceledException"/>
+    public abstract Task<TEntity> ParseAsync(string inputString, CancellationToken token);
 }
